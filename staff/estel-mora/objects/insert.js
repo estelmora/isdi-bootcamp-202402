66fa977aa47513410b3c2c49faae092516a38e7f
Estel <estel@MacBook-Air-de-Estel.local>
2024-02-22 14:49:16 +0100

    add wip #208

--- staff/estel-mora/objects/insert.js
+++ staff/estel-mora/objects/insert.js
@@ -1,20 +1,18 @@
 /**
- * Adds an element in an iterable object.
+ * Inserts an element in iterable object at specfified index.
  *
  * @param object - The iterable object to mutate. 
- * @param value - The value to add.
+ * @param index - The index from which to insert the given values.
+ * @param value - The value to insert.
  * 
- * @throws {TypeError} When object is not an object.
+ * @throws {TypeError} When object is not an object, or when index is not a number.
  */
-function add(object, value) {
-    object[object.length] = value
-
-    object.length++
-
-    return object.length
+function insertMany(object, index, value) {
+    // TODO
+    if (!(object instanceof Object)) throw new TypeError(object + ' is not an Object')
 }
 
-console.log('CASE 1: add violet in colors')
+console.log('CASE 1: insert skyblue in index 1')
 
 var colors = {
     0: 'red',
@@ -23,7 +21,7 @@ var colors = {
     length: 3
 }
 
-var length = add(colors, 'violet')
+var length = insertMany(colors, 1, 'skyblue')
 
 console.log(length)
 // 4
@@ -32,9 +30,70 @@ console.log(colors)
 /*
 {
     0: 'red',
-    1: 'blue',
-    2: 'green',
-    3: 'violet',
+    1: 'skyblue',
+    2: 'blue',
+    3: 'green',
     length: 4
 }
 */
+
+console.log('CASE 2: insert skyblue, gold and plum in index 2')
+
+var colors = {
+    0: 'red',
+    1: 'blue',
+    2: 'green',
+    length: 3
+}
+
+var length = insertMany(colors, 2, 'skyblue', 'gold', 'plum')
+
+console.log(length)
+// 6
+
+console.log(colors)
+/*
+{
+    0: 'red',
+    1: 'blue',
+    2: 'skyblue',
+    3: 'gold',
+    4: 'plum',
+    5: 'green',
+    length: 6
+}
+*/
+
+console.log('CASE 3: fails on undefind object parameter')
+
+try {
+    insertMany()
+} catch (error) {
+    console.log(error)
+    // TypeError: undefined is not an Object
+}
+
+console.log('CASE 4: fails on 1 as an object parameter')
+
+try {
+    insertMany(1)
+} catch (error) {
+    console.log(error)
+    // TypeError: 1 is not an Object
+}
+
+console.log('CASE 5: fails on undefined as index parameter')
+
+var colors = {
+    0: 'red',
+    1: 'blue',
+    2: 'green',
+    length: 3
+}
+
+try {
+    insertMany(colors)
+} catch (error) {
+    console.log(error)
+    // TypeError: undefined is not a Number
+}
