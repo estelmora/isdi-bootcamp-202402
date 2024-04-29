7f404cf9126e7881217ee93fc953de1c4e9808f8
Estel <estel@MacBook-Air-de-Estel.local>
2024-04-29 18:32:52 +0200

    working on showing the podcasts on the landing page #473

--- staff/estel-mora/sonarcast/api/src/index.ts
+++ staff/estel-mora/sonarcast/api/src/index.ts
@@ -4,6 +4,7 @@ dotenv.config({ path: __dirname + '/.env' });
 
 import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors'
 
 import Podcast from './models/Podcast';
 
@@ -11,7 +12,20 @@ const PORT = 5001;
 
 const app = express();
 
+app.use(
+    cors({
+        origin: "*"
+    })
+);
 app.use(express.json());
+
+app.get('/podcasts', async (req: Request, res: Response) => {
+    //fetch podcasts from mongo
+    const podcasts = await Podcast.find();
+    //send back the array to the ui
+    res.json(podcasts)
+
+})
 
 app.post('/podcasts', async (req: Request, res: Response) => {
     const newPodcast = new Podcast({
--- staff/estel-mora/sonarcast/app/src/App.tsx
+++ staff/estel-mora/sonarcast/app/src/App.tsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+type TPodcasts = {
+    title: string;
+    _id: string;
 }
 
-export default App
+
+function App() {
+    const [podcasts, setPodcasts] = useState<TPodcasts[]>([]);
+    const [transcript, setTranscript] = useState('');
+
+    async function handleUploadPodcast(e: React.FormEvent) {
+        e.preventDefault();
+        await fetch('http://localhost:5001/podcasts', {
+            method: 'POST',
+            body: JSON.stringify({
+                transcript,
+            }),
+            headers: {
+                "Content-Type": 'application/json',
+            }
+        })
+        setTranscript("");
+    }
+
+    useEffect(() => {
+        async function fetchPodcasts() {
+            const response = await fetch("http://localhost:5001/podcasts")
+            const newPodcasts = await response.json();
+            setPodcasts(newPodcasts);
+            console.log(newPodcasts)
+        }
+        fetchPodcasts();
+
+
+    }, []);
+
+
+
+    return (
+        <div className="App">
+            <ul className='podcasts'>
+                {podcasts.map((podcast) => (
+                    <li key={podcast._id}>{podcast.transcript}</li>
+
+                ))}
+            </ul>
+
+
+
+            <form onSubmit={handleUploadPodcast}>
+                <label htmlFor="podcast-transcript">Podcast Transcript</label>
+                <input
+                    id="podcast-transcript"
+                    value={transcript}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        setTranscript(e.target.value);
+                    }}
+                />
+                <button>Upload Podcast</button>
+            </form>
+        </div>
+    )
+}
+
+export default App;
