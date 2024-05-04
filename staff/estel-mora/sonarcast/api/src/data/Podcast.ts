85a288a293ffa607380d28d7856b01049ffefa2e
Estel <estel@MacBook-Air-de-Estel.local>
2024-05-04 12:21:28 +0200

    add podcast.date attribute and render it on page #473

--- staff/estel-mora/sonarcast/api/src/data/Podcast.ts
+++ staff/estel-mora/sonarcast/api/src/data/Podcast.ts
@@ -3,13 +3,37 @@ import mongoose, { ObjectId } from 'mongoose';
 const Schema = mongoose.Schema
 const ObjectId = mongoose.Types.ObjectId
 
-const PodcastSchema = new Schema({
-    title: String,
-    transcript: String,
+type PodcastType = {
+    title: string,
+    transcript: string,
+    ideas: string,
     date: Date,
     author: ObjectId,
+}
+
+const PodcastSchema = new Schema<PodcastType>({
+    title: {
+        type: String,
+        required: true
+    },
+    transcript: {
+        type: String,
+        required: true
+    },
+    ideas: {
+        type: String,
+        required: false
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    author: {
+        type: ObjectId,
+        required: false // need to change after adding auth
+    }
 })
 
-const PodcastModel = mongoose.model("Podcast", PodcastSchema);
+const Podcast = mongoose.model("Podcast", PodcastSchema);
 
-export default PodcastModel
+export default Podcast
--- staff/estel-mora/sonarcast/app/src/pages/Podcast.tsx
+++ staff/estel-mora/sonarcast/app/src/pages/Podcast.tsx
@@ -5,18 +5,28 @@ import axios from "axios";
 import Navbar from "../components/Navbar";
 import { API_URL } from "../logic/config";
 
+interface Podcast {
+    title: string;
+    transcript: string;
+    date: Date | null;
+}
+
 export default function Podcast() {
 
     const { podcastId } = useParams();
-    const [podcast, setPodcast] = useState({
+    const [podcast, setPodcast] = useState<Podcast>({
         title: '',
-        transcript: ''
+        transcript: '',
+        date: null
     })
 
     useEffect(() => {
         const fetchPodcast = async () => {
             try {
                 const { data } = await axios.get(`${API_URL}/podcasts/${podcastId}`)
+                if (data.date) {
+                    data.date = new Date(data.date);
+                }
                 setPodcast(data)
             } catch (error) {
                 console.error('Error fetching podcast:', error)
@@ -26,18 +36,25 @@ export default function Podcast() {
         fetchPodcast()
     }, [podcastId])
 
+    async function generateIdeas() {
+        // Cridar OpenAI amb podcast.transcript i prompt
+        // Fer PATCH a /podcast/{podcastId} amb { ideas: ideas }
+    }
+
     return (
         <div>
             <Navbar />
             <div className="Podcast">
                 <h1>{podcast.title}</h1>
-                <h2>Date</h2>
+                <h2>{podcast.date
+                    ? podcast.date.toDateString()
+                    : "Loading date..."}</h2>
                 <div className="transcript-container">
                     <h2>Transcript</h2>
                     <div className="transcript-text">{podcast.transcript}</div>
                 </div>
                 <div className="ideas">Ideas
-                    <button>generate ideas</button>
+                    {/* <button onClick={generateIdeas()}>generate ideas</button> */}
                     <div className="ideas-list">ideas list</div>
                 </div>
 
